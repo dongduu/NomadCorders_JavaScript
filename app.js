3e8df0d7930dc7979ff0967c852cc214e9372583
dongduu <4012popo@naver.com>
2021-09-17 21:23:12 +0900

    [노마드 코더_바닐라 JS로 크롬 앱 만들기] add : const와 let

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const a = 5;
+const a = 5; // const: 상수(값 업데이트 불가능)
 const b = 2;
-const myName = "dongdu";
+let myName = "dongdu"; // let: 변수(나중에 바꿀 수 있음)
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("Hello " + myName);
+
+myName = "dudu"; // let은 처음 생성할 때만 사용
+
+console.log("your new name is " + myName); 
