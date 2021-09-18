2d601dafb1c932a9a93b3f25078ed36609078b50
dongduu <4012popo@naver.com>
2021-09-18 19:49:03 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Functions part 2.

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
-function sayHello() {
-    console.log("Hello my name is a");
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + " and I'm " + age);
 }
 
-sayHello("dongdu");
-sayHello("nico");
-sayHello("hyun6ick");
+sayHello("dongdu", 22);
+sayHello("nico", 10);
+sayHello("hyun6ick", 27);
+
+function plus(a, b) {
+    console.log(a + b);
+}
+
+plus(12, 90);
+plus("dongdu ", "is pretty");
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+divide(80, 8);
+
+const player = {
+    name: "dd",
+    sayBye: function (othersName) {
+        console.log("Bye! " + othersName);
+    },
+};
+player.sayBye("bb");
