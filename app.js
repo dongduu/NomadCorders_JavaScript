4798b11be2ae360cd26ed26e913f5a5c138d1c1a
dongduu <4012popo@naver.com>
2021-09-19 23:56:48 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Returns

--- app.js
+++ app.js
@@ -1,22 +1,25 @@
 const calculator = {
     plus: function (a, b) {
-        console.log (a + b);
+        return a + b;
     },
     minus: function (a, b) {
-        console.log (a - b);
+        return a - b;
     },
     divide: function (a, b) {
-        console.log (a / b);
+        return a / b;
     },
     multi: function (a, b) {
-        console.log (a * b);
+        return a * b;
     },
     powerOf: function (a, b) {
-        console.log (a ** b);
+        return a ** b;
     },
 };
 
-calculator.plus (12, 43);
-calculator.minus (33, 13);
-calculator.divide (50, 5);
-calculator.powerOf (2, 4);
+const resultA = calculator.plus (12, 43);
+const resultB = calculator.minus (resultA, 10);
+const resultC = calculator.divide (resultB, 5); // 상호의존 상태
+
+console.log(resultA); // 외부에서 사용
+console.log(resultB);
+console.log(resultC);
