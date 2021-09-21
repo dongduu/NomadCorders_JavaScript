9ee0f7daec515dec1faa556308037ac40fe197db
dongduu <4012popo@naver.com>
2021-09-21 21:08:01 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Conditions part Two

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-const age = prompt("몇 살 입니까?");
-console.log(parseInt(age)); // 사용자가 숫자가 아닌 값을 입력했을 때 인지 할 수 있음
-// const age = parseInt(prompt("몇 살 입니까?"));
-// console.log (age);
+const age = parseInt(prompt("몇 살 입니까?"));
+
+if (isNaN(age)) {
+    console.log("Please write a number.");
+} else {
+    console.log("Thank you for writing your age.");
+}
