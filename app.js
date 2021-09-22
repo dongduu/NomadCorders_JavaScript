91a7d07acd8033b44377b58afcd13278940dc7a9
dongduu <4012popo@naver.com>
2021-09-22 16:19:37 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Searching For Elements

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-const title = document.getElementById("title");
-
-title.innerText = "Got you";
-
-console.log(title.id);
-console.log(title.className);
-console.log(title.innerText);
-console.log(title.textContent);
-
-console.dir(title)
+const Hellos = document.querySelector(".hello h1"); // CSS 셀러터/CSS처럼 부를 수 있음(hello라는 class 안에 있는 h1만 부름), get은 모두 부를 수 이따
+const Helloss = document.querySelectorAll(".hello h1");
+console.log(Hellos);
+console.log(Helloss);
