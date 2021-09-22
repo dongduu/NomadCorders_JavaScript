f61e48c11a137136f7890ee4d515bf8e4ec7d7b4
dongduu <4012popo@naver.com>
2021-09-22 15:53:06 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : HTML in Javascript

--- app.js
+++ app.js
@@ -1,15 +1,10 @@
-const age = parseInt(prompt("몇 살 입니까?"));
+const title = document.getElementById("title");
 
-if (isNaN(age) || age < 0) {
-    console.log("Please write a real positive number.");
-} else if (age < 18) {
-    console.log("you are too young.");
-} else if (age >= 18 && age <= 50) {
-    console.log("You can drink.");
-} else if (age > 50 && age <= 80) {
-    console.log("You should exercise.");
-} else if (age === 100) {
-    console.log("Wow, you are wise.")
-} else if (age > 80) {
-    console.log("You can do whatever you want.");
-} 
+title.innerText = "Got you";
+
+console.log(title.id);
+console.log(title.className);
+console.log(title.innerText);
+console.log(title.textContent);
+
+console.dir(title)
