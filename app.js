39c19dce6ad47a4b7ce845a6d7e146a325559bc6
dongduu <4012popo@naver.com>
2021-09-22 16:59:27 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Events

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-const Hellos = document.querySelector(".hello h1"); // CSS 셀러터/CSS처럼 부를 수 있음(hello라는 class 안에 있는 h1만 부름), get은 모두 부를 수 이따
-const Helloss = document.querySelectorAll(".hello h1");
-console.log(Hellos);
-console.log(Helloss);
+const title = document.querySelector(".hello h1"); // CSS 셀러터/CSS처럼 부를 수 있음(hello라는 class 안에 있는 h1만 부름), get은 모두 부를 수 이따
+
+console.dir(title); // Element의 내부를 보고 싶을 떄 사용
+console.log(title);
+
+function handleTilteClick () {
+    console.log("title was clicked!");
+    title.style.color = "green";
+}
+
+title.addEventListener("click", handleTilteClick); // 클릭 했을 때 핸들 함수가 실행(함수에 괄호를 넣으면 안 됨)
