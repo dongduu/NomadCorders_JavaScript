a7ab729b9033515afebe8072dd2517aa4214bb7d
dongduu <4012popo@naver.com>
2021-09-23 21:40:40 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Events

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
 
-function LoginBtnClick () {
-    const username = loginInput.value;
-    console.log(username);
+function loginSubmit (event) {
+    event.preventDefault();
+    console.log(event);
 };
 
-loginButton.addEventListener("click", LoginBtnClick);
+loginForm.addEventListener("submit", loginSubmit); // submit은 엔터를 누르거나 버튼을 누를 떄 작동
