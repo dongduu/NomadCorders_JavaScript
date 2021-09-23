d535934b441682debe5aa234571b6a3a8990c25f
dongduu <4012popo@naver.com>
2021-09-23 19:26:55 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Input Values

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const h1 = document.querySelector(".hello h1"); // CSS 셀러터/CSS처럼 부를 수 있음(hello라는 class 안에 있는 h1만 부름), get은 모두 부를 수 이따
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
 
-function handleTilteClick () {
-    h1.classList.toggle("clicked");
-}
+function LoginBtnClick () {
+    console.dir(loginInput.value);
+    console.log("Click!!!");
+};
 
-h1.addEventListener ("click", handleTilteClick); 
+loginButton.addEventListener("click", LoginBtnClick);
