c59047c41b92b8d5b1a0998fa61b7b7b398dcfa7
dongduu <4012popo@naver.com>
2021-09-23 17:29:38 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : CSS in Javascript part Two

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const h1 = document.querySelector(".hello h1"); // CSS 셀러터/CSS처럼 부를 수 있음(hello라는 class 안에 있는 h1만 부름), get은 모두 부를 수 이따
 
 function handleTilteClick () {
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "blue") {
-        newColor = "tomato";
+    const clickedClass = "clicked"
+    if (h1.className === clickedClass) {
+        h1.className = "";
     } else {
-        newColor ="blue";
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
 
 h1.addEventListener ("click", handleTilteClick); 
