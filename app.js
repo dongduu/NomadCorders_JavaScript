671977b5ac0bf06458f16d2aa6866ebf3692a02e
dongduu <4012popo@naver.com>
2021-09-24 14:15:27 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Getting Username

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
 
 function loginSubmit (event) {
     event.preventDefault();
-    console.log(event);
-};
-
-function linkClick (event) {
-    event.preventDefault();
-    console.dir(event);
-    // alert("Clicked"); => alert가 페이지 이동하는 것을 막아줌 -> 확인 버튼 클릭 -> 페이지 이동
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 };
 
 loginForm.addEventListener("submit", loginSubmit); // submit은 엔터를 누르거나 버튼을 누를 떄 작동
-link.addEventListener("click", linkClick);
