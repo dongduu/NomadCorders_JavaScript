8b3b9653c7423d70cd9667fb5522a78818dfbd0f
dongduu <4012popo@naver.com>
2021-09-24 17:32:45 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Loading Username

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function loginSubmit (event) {
     event.preventDefault();
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintingGreeting (username);
+};
+
+function paintingGreeting (username) {
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 };
 
-loginForm.addEventListener("submit", loginSubmit); // submit은 엔터를 누르거나 버튼을 누를 떄 작동
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", loginSubmit);
+    // show the form
+} else {
+    paintingGreeting (savedUsername);
+    // show the greeting
+}
