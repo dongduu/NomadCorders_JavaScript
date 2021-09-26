11f852de3c1f5c43b475d774fcad857c782594f8
dongduu <4012popo@naver.com>
2021-09-26 23:00:48 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Background

---
+++ background.js
@@ -0,0 +1,9 @@
+const images = ["쿠로미1.jpg", "쿠로미2.jpg", "쿠로미3.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
