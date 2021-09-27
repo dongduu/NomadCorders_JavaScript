ae030e7f8c592dfce507e4851becc58f5cba70c9
dongduu <4012popo@naver.com>
2021-09-28 01:14:21 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Saving To Dos

--- todo.js
+++ todo.js
@@ -1,6 +1,11 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
+const toDos = [];
+
+function saveTodos () {
+    localStorage.setItem("toDos", JSON.stringify(toDos)); // JSON.stringify => 배열로 바꿔줌
+};
 
 function deleteTodo (event) {
     const liRemove = event.target.parentElement;
@@ -23,7 +28,9 @@ function toDoSubmit (event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintTodo(newTodo);
+    saveTodos();
 };
 
 toDoForm.addEventListener("submit", toDoSubmit);
