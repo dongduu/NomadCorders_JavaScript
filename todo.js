49484b74298d3c0ba699fad7ea9d69409ce96ab0
dongduu <4012popo@naver.com>
2021-09-28 00:07:53 +0900

    [노마드코더_바닐라 JS로 크롬 앱 만들기] add : Adding ToDos

--- todo.js
+++ todo.js
@@ -2,10 +2,19 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function paintTodo (newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span); // li의 자식태그가 생김
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+};
+
 function toDoSubmit (event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    paintTodo(newTodo);
 };
 
 toDoForm.addEventListener("submit", toDoSubmit);
